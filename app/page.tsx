5c41544d49ee371ecc1231f2a3a41dbd2da19a1e
webclues <webclues@webclues-keyur-sachaniya.tail5d8504.ts.net>
2026-05-15 14:14:59 +0530

    Fix: Added suppressHydrationWarning on <html>, <body>
    That tells React to ignore attribute differences on those nodes during hydration

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div
+      className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black"
+      suppressHydrationWarning
+    >
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
